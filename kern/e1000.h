cd5c8deccb21f05c7738dcdf81a6278f565eca29
Bvsk Patnaik <bvsaikiranpatnaik@gmail.com>
2019-01-02 00:08:08 +0530

    Lab 6 Excercise 3

--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,10 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+// Forward declare
+struct pci_func;
+
+void pci_func_enable(struct pci_func *pcif);
+int attach (struct pci_func *pcif);
+
 #endif  // SOL >= 6
