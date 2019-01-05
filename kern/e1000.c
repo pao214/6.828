611752ca66ac4337e406f6a35a9790010b9465f2
Bvsk Patnaik <bvsaikiranpatnaik@gmail.com>
2019-01-05 15:15:54 +0530

    Lab 6 Excercise 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,12 +3,63 @@
 
 // LAB 6: Your driver code here
 
-volatile uint32_t *netaddr;
+volatile uint8_t *netaddr;
+struct e1000_tx_desc_t tx_descs[E1000_TX_DESC_LEN] __attribute__ ((aligned (16)));
+uint8_t tx_buf[E1000_TX_DESC_LEN*E1000_MAX_PKT_LEN];
 
 int attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
     netaddr = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-    assert(*(netaddr+2) == E1000_FULL_DUPLEX);
+    validate();
+    tx_init();
     return 1;
+}
+
+void tx_init()
+{
+    // Device only accepts physical contiguous memory
+    *(uint32_t *)(netaddr+E1000_TDBAL) = (uintptr_t)PADDR(tx_descs);
+    *(uint32_t *)(netaddr+E1000_TDLEN) = sizeof(tx_descs);
+    *(uint16_t *)(netaddr+E1000_TDH) = 0;
+    *(uint64_t *)(netaddr+E1000_TDT) = 0;
+
+    // Initialize descriptors
+    for (int i = 0; i < E1000_TX_DESC_LEN; i++)
+    {
+        struct e1000_tx_desc_t *tx_desc = &tx_descs[i];
+        memset(tx_desc, 0, sizeof(*tx_desc));
+        tx_desc->addr = (uint64_t)(PADDR(&tx_buf[i*E1000_MAX_PKT_LEN]));
+        // tx_desc->length <= E1000_MAX_PKT_LEN;
+        // tx_desc->cmd.eop
+        tx_desc->cmd.rs = 1;
+        // tx_desc->cmd.dext already set to zero
+    }
+    
+    // Initialize TCTL
+    struct e1000_tctl_t tctl;
+    memset(&tctl, 0, sizeof(tctl));
+    tctl.en = 1;
+    tctl.psp = 1;
+    tctl.ct = 0x10;
+    tctl.cold = 0x40;
+    *(struct e1000_tctl_t *)(netaddr+E1000_TCTL) = tctl;
+
+    // Initialize TIPG
+    struct e1000_tipg_t tipg;
+    memset(&tipg, 0, sizeof(tipg));
+    tipg.ipgt = 10;
+    tipg.ipgr1 = 8;
+    tipg.ipgr2 = 6;
+    *(struct e1000_tipg_t *)(netaddr+E1000_TIPG) = tipg;
+}
+
+void validate()
+{
+    assert(*(uint32_t *)(netaddr+E1000_STATUS) == E1000_FULL_DUPLEX);
+    assert(sizeof(struct e1000_tctl_t) == 4);
+    assert(sizeof(struct e1000_tipg_t) == 4);
+    assert(sizeof(struct e1000_tx_desc_t) == 16);
+    assert(!(sizeof(tx_descs)&0x7f));
+    assert(!((uintptr_t)PADDR(tx_descs)&0xf));
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,14 +1,82 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <inc/string.h>
 #include <kern/pci.h>
 
 #define E1000_VEN_ID_82540EM                0x8086
 #define E1000_DEV_ID_82540EM                0x100E
 #define E1000_FULL_DUPLEX                   0x80080783
+#define E1000_TX_DESC_LEN                   0x40
+#define E1000_MAX_PKT_LEN                   0x5EE
 
-extern volatile uint32_t *netaddr;
+// Register set
+#define E1000_STATUS                        0x00008     // Device status - RO
+#define E1000_TCTL                          0x00400     // TX control - RW
+#define E1000_TIPG                          0x00410     // TX inter-packet gap - RW
+#define E1000_TDBAL                         0x03800     // TX descriptor base addr - RW
+#define E1000_TDLEN                         0x03808     // TX descriptor size - RW
+#define E1000_TDH                           0x03810     // TX descriptor head - RW
+#define E1000_TDT                           0x03818     // TX descritor tail - RW
+
+#pragma pack(push, 1)
+
+// TCTL register
+struct e1000_tctl_t
+{
+    unsigned rsv    : 1;
+    unsigned en     : 1;
+    unsigned rsv2   : 1;
+    unsigned psp    : 1;
+    unsigned ct     : 8;
+    unsigned cold   : 10;
+    unsigned swxoff : 1;
+    unsigned rsv3   : 1;
+    unsigned rtlc   : 1;
+    unsigned rsv4   : 1;
+    unsigned rsv5   : 6;
+};
+
+// TIPG register
+struct e1000_tipg_t
+{
+    unsigned ipgt   : 10;
+    unsigned ipgr1  : 10;
+    unsigned ipgr2  : 10;
+    unsigned rsv    : 2;
+};
+
+// Descriptor command
+struct e1000_tx_cmd_t
+{
+    unsigned eop    : 1;
+    unsigned ifcs   : 1;
+    unsigned ic     : 1;
+    unsigned rs     : 1;
+    unsigned rsv    : 1;
+    unsigned dext   : 1;
+    unsigned vle    : 1;
+    unsigned ide    : 1;
+};
+
+// Transmit descriptor
+struct e1000_tx_desc_t
+{
+    uint64_t                addr;
+    uint16_t                length;
+    uint8_t                 cso;
+    struct e1000_tx_cmd_t   cmd;
+    uint8_t                 status;
+    uint8_t                 css;
+    uint16_t                special;
+};
+
+#pragma pack(pop)
+
+extern volatile uint8_t *netaddr;
 
 int attach(struct pci_func *pcif);
+void tx_init();
+void validate();
 
 #endif  // SOL >= 6
